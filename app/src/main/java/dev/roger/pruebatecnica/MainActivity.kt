cf4b03ac50798c7c7ea494dc2089ed9ffbf89e92
Roger <rogerdeumarti@gmail.com>
2022-04-26 20:16:38 +0200

    Initialized Repository

---
+++ app/src/main/java/dev/roger/pruebatecnica/MainActivity.kt
@@ -0,0 +1,13 @@
+package dev.roger.pruebatecnica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
